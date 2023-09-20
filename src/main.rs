3df536f3abb7bcc215540b4bdc4da22e890a73d1
Nathan Federknopp <nknopp@gmail.com>
2023-09-20 00:04:55 -0700

    Sort of working

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@ use esp_idf_hal::prelude::Peripherals;
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
     nvs::EspDefaultNvsPartition,
-    timer::EspTaskTimerService,
 };
+use embedded_svc::wifi::ClientConfiguration;
 use std:: {
     thread,
     time::Duration,
+    time,
 };
 
 // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
@@ -26,14 +27,35 @@ fn main() -> Result<()> {
     let peripherals = Peripherals::take().unwrap();
     let sysloop = EspSystemEventLoop::take()?;
     let nvs = EspDefaultNvsPartition::take()?;
-    let timer_service = EspTaskTimerService::new()?;
 
     // Connect to the Wi-Fi network
-    let _wifi_handle = wifi::run_wifi_service(peripherals.modem, sysloop, nvs, timer_service)?;    
+    let _wifi_svc = wifi::WifiService::run_wifi_service(peripherals.modem, sysloop, nvs)?;    
 
     let _server = server::init_server()?;    
 
+
+    let cur_time = time::Instant::now();
+    let mut changed_mode = false;
+    let mut changed_mode_2 = false;
     loop {
+        if !changed_mode && cur_time.elapsed() > Duration::from_secs(10) {
+            println!("Changing to client");
+            _wifi_svc.set_wifi_mode(wifi::WifiMode::Client(ClientConfiguration {
+                ssid: "RociLANte".into(),
+                password: "RememberTheCant".into(),
+                ..Default::default()
+            }));
+            changed_mode = true;
+        }
+        if !changed_mode_2 && cur_time.elapsed() > Duration::from_secs(20) {
+            println!("Changing to bad client");
+            _wifi_svc.set_wifi_mode(wifi::WifiMode::Client(ClientConfiguration {
+                ssid: "RociLANte".into(),
+                password: "RememberTheCant2".into(),
+                ..Default::default()
+            }));
+            changed_mode_2 = true;
+        }
         thread::sleep(Duration::from_millis(1000));
     }
 }
--- src/wifi.rs
+++ src/wifi.rs
@@ -1,10 +1,9 @@
 use anyhow::Result;
 use esp_idf_hal::peripheral;
 use esp_idf_svc::{
-    eventloop::{EspSystemEventLoop, EspSubscription, System, Wait},
+    eventloop::EspSystemEventLoop,
     wifi::{EspWifi, BlockingWifi, WifiEvent, WifiDriver},
     nvs::{EspNvsPartition, NvsDefault},
-    timer::EspTaskTimerService,
     netif::{NetifConfiguration, EspNetif, NetifStack},
 };
 use embedded_svc::{
@@ -18,6 +17,7 @@ use embedded_svc::{
 use std:: {
     thread,
     time::Duration,
+    sync::{Arc, Mutex},
 };
 
 
@@ -25,52 +25,88 @@ const MAX_RETRIES: u8 = 10;
 const AP_SSID: &str = "ESP32";
 const AP_PW: &str = "";
 
+const WIFI_MODE_MUT_ERR: &str = "Failure to unlock WifiMode mutex";
 
-enum WifiMode {
+
+#[derive(Clone, Debug)]
+pub enum WifiMode {
     AP,
-    Client,
+    Client(ClientConfiguration),
+}
+
+enum ClientWatchdogResult {
+    Continue,
+    SwitchMode(WifiMode),
 }
 
 
-pub fn run_wifi_service(
-    modem: impl peripheral::Peripheral<P = esp_idf_hal::modem::Modem> + 'static,
-    sysloop: EspSystemEventLoop,
-    nvs: EspNvsPartition<NvsDefault>,
-    timer_service: EspTaskTimerService,
-) -> Result<thread::JoinHandle<()>> {
-
-    // AP IP config
-    let ipv4_cfg = ipv4::RouterConfiguration {
-        subnet: ipv4::Subnet {
-            gateway: ipv4::Ipv4Addr::new(192, 168, 1, 1),
-            mask: ipv4::Mask(24) // equivalent to 255.255.255.0
-        },
-        ..Default::default()
-    };
-
-    let net_conf = NetifConfiguration {
-        ip_configuration: embedded_svc::ipv4::Configuration::Router(ipv4_cfg),
-        ..NetifConfiguration::wifi_default_router()
-    };
-
-    let driver = WifiDriver::new(modem, sysloop.clone(), Some(nvs))?;
-    let esp_wifi = EspWifi::wrap_all(
-        driver,
-        EspNetif::new(NetifStack::Sta)?,
-        EspNetif::new_with_conf(&net_conf)?,
-    )?;
-
-    Ok(thread::spawn(move || {
-        if let Err(e) = wifi_service_start(esp_wifi, sysloop) {
-            log::error!("Error running wifi service: {e:?}");
-        }
-    }))
+pub struct WifiService {
+    _handle: thread::JoinHandle<()>,
+    wifi_mode: Arc<Mutex<WifiMode>>,
 }
+
+
+impl WifiService {
+    pub fn run_wifi_service(
+        modem: impl peripheral::Peripheral<P = esp_idf_hal::modem::Modem> + 'static,
+        sysloop: EspSystemEventLoop,
+        nvs: EspNvsPartition<NvsDefault>,
+    ) -> Result<Self>
+    {
+        // AP IP config
+        let ipv4_cfg = ipv4::RouterConfiguration {
+            subnet: ipv4::Subnet {
+                gateway: ipv4::Ipv4Addr::new(192, 168, 1, 1),
+                mask: ipv4::Mask(24) // equivalent to 255.255.255.0
+            },
+            ..Default::default()
+        };
+
+        let net_conf = NetifConfiguration {
+            ip_configuration: embedded_svc::ipv4::Configuration::Router(ipv4_cfg),
+            ..NetifConfiguration::wifi_default_router()
+        };
+
+        let driver = WifiDriver::new(modem, sysloop.clone(), Some(nvs))?;
+        let esp_wifi = EspWifi::wrap_all(
+            driver,
+            EspNetif::new(NetifStack::Sta)?,
+            EspNetif::new_with_conf(&net_conf)?,
+        )?;
+
+        let wifi_mode = Arc::new(Mutex::new(WifiMode::AP));
+        let wifi_mode_c = wifi_mode.clone();
+
+        let join_handle = thread::spawn(move || {
+            if let Err(e) = wifi_service_start(esp_wifi, sysloop, wifi_mode_c) {
+                log::error!("Error running wifi service: {e:?}");
+            }
+        });
+
+        Ok(Self {
+            _handle: join_handle,
+            wifi_mode,
+        })
+    }
+
+    pub fn set_wifi_mode(&self, mode: WifiMode) {
+        log::info!("Setting wifi mode to {mode:?}");
+        *self.wifi_mode.lock().expect(WIFI_MODE_MUT_ERR) = mode;
+    }
+
+    pub fn get_wifi_mode(&self) -> WifiMode {
+        self.wifi_mode.lock().expect(WIFI_MODE_MUT_ERR).to_owned()
+    }
+}
+
+
+
 
 
 fn wifi_service_start(
     mut esp_wifi: EspWifi,
     sysloop: EspSystemEventLoop,
+    wifi_mode: Arc<Mutex<WifiMode>>,
 ) -> Result<()>
 {
     // FOR DEBUGGING
@@ -79,43 +115,64 @@ fn wifi_service_start(
         on_wifi_event(event);
     })?;
 
-    
-
     let mut wifi = BlockingWifi::wrap(&mut esp_wifi, sysloop.clone())?;
 
-    let mut wifi_mode = match wifi.get_configuration()? {
-        Configuration::Client(config) => {
-            if config.ssid.is_empty() {
-                // No saved ssid
-                WifiMode::AP
-            } else {
-                // Saved ssid, so let's try using that
-                log::info!("Will try to connect to previous SSID: {}", config.ssid);
-                WifiMode::Client
-            }
-        },
-        // Either no config or in AP mode already
-        _ => WifiMode::AP
-    };
+    *wifi_mode.lock().expect(WIFI_MODE_MUT_ERR) = 
+        match wifi.get_configuration()? {
+            Configuration::Client(config) => {
+                if config.ssid.is_empty() {
+                    // No saved ssid
+                    WifiMode::AP
+                } else {
+                    // Saved ssid, so let's try using that
+                    log::info!("Will try to connect to previous SSID: {}", config.ssid);
+                    WifiMode::Client(config)
+                }
+            },
+            // Either no config or in AP mode already
+            _ => WifiMode::AP
+        };
 
     log::info!("Starting wifi watchdog");
     loop {
-        match wifi_mode {
-            WifiMode::Client => {
-                while matches!(wifi_mode, WifiMode::Client) {
-                    client_watchdog(&mut wifi, &mut wifi_mode)?;
+        let cur_wifi_mode = wifi_mode.lock().expect(WIFI_MODE_MUT_ERR).to_owned();
+        match cur_wifi_mode {
+            WifiMode::Client(config) => {
+                log::info!("Client watchdog");
+                wifi.stop()?;
+                wifi.set_configuration(&Configuration::Client(config.clone()))?;
+
+                loop {
+                    if !wifi.is_up()? {
+                        log::info!("Wifi client disconnected. Attempting to connect");
+                        let status = perform_client_connection(&mut wifi)?;
+                        if let ClientWatchdogResult::SwitchMode(mode) = status {
+                            *wifi_mode.lock().expect(WIFI_MODE_MUT_ERR) = mode;
+                            break
+                        }
+                    }
+                    if !matches!(&mut *wifi_mode.lock().expect(WIFI_MODE_MUT_ERR), WifiMode::Client(_)) {
+                        log::info!("No longer in Client mode");
+                        break
+                    }
                     thread::sleep(Duration::from_millis(1000));
                 }
             },
             WifiMode::AP => {
+                log::info!("AP watchdog");
                 activate_ap(&mut wifi)?;
-                while wifi.is_up()? && matches!(wifi_mode, WifiMode::AP) {
+                loop {
+                    if !wifi.is_up()? || !matches!(&mut *wifi_mode.lock().expect(WIFI_MODE_MUT_ERR), WifiMode::AP) {
+                        log::info!("No longer in AP mode or had a disconnection");
+                        break
+                    }
                     thread::sleep(Duration::from_millis(1000));
                 }
             }
         }
         
     }
+    log::warn!("Exiting wifi watchdog");
 }
 
 
@@ -124,13 +181,7 @@ fn on_wifi_event(event: &WifiEvent) {
 }
 
 
-fn client_watchdog(wifi: &mut BlockingWifi<&mut EspWifi>, wifi_mode: &mut WifiMode) -> Result<()> {
-    if wifi.is_up()? {
-        return Ok(())
-    }
-
-    log::info!("Wifi client disconnected. Attempting to reestablish connection");
-    
+fn perform_client_connection(wifi: &mut BlockingWifi<&mut EspWifi>) -> Result<ClientWatchdogResult> {    
     let mut retries = 0;
 
     while retries < MAX_RETRIES {
@@ -161,15 +212,13 @@ fn client_watchdog(wifi: &mut BlockingWifi<&mut EspWifi>, wifi_mode: &mut WifiMo
             }
         }
         if wifi.is_up()? {
-            return Ok(())
+            return Ok(ClientWatchdogResult::Continue)
         }
     }
 
     // Ran out of retries
     log::warn!("Error connecting to wifi client. Returning to AP mode");
-    *wifi_mode = WifiMode::AP;
-
-    Ok(())
+    Ok(ClientWatchdogResult::SwitchMode(WifiMode::AP))
 }
 
 
